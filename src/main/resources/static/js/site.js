027b15922b29877c153c7be947b6821fe67f8410
Brahmani <brahmanitvs@gmail.com>
2026-05-27 23:04:12 +0530

    added navbar

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -1,7 +1,7 @@
 console.log("Hello from js");
 var users = [
-    {"name" : "John", "gender" : "Male", "img" : "img/john.png"},
-    {"name" : "Jane", "gender" : "Female", "img" : "img/jane.png"}
+    {"name" : "John", "gender" : "Male", "img" : "https://www.shutterstock.com/image-vector/portrait-girl-beautiful-long-hair-260nw-1969681783.jpg"},
+    {"name" : "Jane", "gender" : "Female", "img" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUkosiVeorClpqBo5kOw6YOHze81n8ObhOZQ&s"}
 ];
 
 var currentUserIndex = 0;
